b89f49759e058134a22ea21e337c244ea2ca2db9
Luke Ugie <ugalinoluke@gmail.com>
2022-04-12 23:22:41 +0000

    Add Notes

---
+++ Notes/Fundamentals.py
@@ -0,0 +1,49 @@
+
+# *Fundamental Data Types
+# int
+# float
+# bool 
+# str 
+# list 
+# tuple 
+# set 
+# dict 
+
+
+# *Variables
+# snake_case
+# Start with lowercase or underscore
+# Letters, numbers, underscores
+# Case sensitive
+# Don't overwrite keywords
+# !Putting an underscore in front usually
+#   !indicates a private variable
+
+user_IQ = 190
+print(user_IQ)
+
+
+# *Operator Precedence
+# !From high to low
+# Parantheses ()
+# Exponents **
+# Multiplication or Division * /
+# Addition or Subtraction + -
+
+# * bin() and complex
+# complex - only use this for very complex math,
+# may not use it ever
+# bin() - displays the binary representation
+# of whatever you use
+
+# *Expressions vs Statements
+# Expression: piece of code that produces
+# a value [only the iq / 5]
+# Statement: an entire line of code [line 45]
+
+iq = 100
+user_age = iq / 5
+
+
+#* Augmented Assignment Operator
+
