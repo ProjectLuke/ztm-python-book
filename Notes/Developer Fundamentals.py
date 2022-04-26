ea71b92401d6898b9d91b6bb4b042c6f18c7658e
Luke Ugie <ugalinoluke@gmail.com>
2022-04-26 19:37:32 +0000

    Add Notes

---
+++ Notes/Developer Fundamentals.py
@@ -0,0 +1,11 @@
+
+#* Understanding Data Structures
+# Good programmer that is able to
+# decide what data structure and when
+
+#Understand the tradeoffs and know when to use what
+#This will take practice and such to understand
+
+#! When to use a List vs Dictionary
+#List has order
+#Dictionary has no order
--- Notes/Fundamentals.py
+++ Notes/Fundamentals.py
@@ -148,3 +148,159 @@ now_cool = True
 # any type
 #li = [1,2,3,4,5]
 #li2 = [1,2,'a', True]
+
+#Keep in mind, each object in the list has a position based on the index
+# numbers
+
+
+
+
+#* List Slicing
+#* [start:stop:step]
+#start position: end position: the increment
+#! Slicing makes lists mutable
+amazon_cart = [
+    'notebooks',
+    'sunglasses',
+    'toys',
+    'grapes'
+]
+amazon_cart[0] = 'laptop'
+print(amazon_cart[0::2])
+
+
+#? To make a copy of a list
+new_cart = amazon_cart[:]
+#new_cart = amazon_cart
+#The above kinda does the same, but anytime amazon_cart is modified or adjusted, the
+# new_cart will also follow because it's referring to the amazon_cart
+#If want to make a copy, make sure to use the [:]
+
+
+
+#* Matrix
+#A way to describe multi-dimensional lists
+matrix = [
+    [1,2,3],
+    [2,4,6],
+    [3,6,9]
+]
+#Basically an array within an array
+#These come up a lot in ML
+
+#? Accessing a Matrix
+matrix[0][1]
+print(matrix[0][1])
+#Output will print out the 2 in the first row
+#Am able to more arrays and do
+matrix[1][2][3][n]
+
+
+
+#* List Methods
+#Python has some methods used for Lists/Arrays
+#? Adding 
+basket = [1,2,3,4,5]
+new_list = basket.append('100')
+print(new_list)
+#! The above modifies the list in place, not produce a result
+#! The below is the right way to do it
+#Run both to see
+basket.insert(4, '100')
+print(basket)
+
+#? Different methods do different things
+#? Google and test to find out
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the first item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
+#* Common List Patterns
+sort()
+reverse()
+join()
+
+
+
+#* List unpacking
+#Ex 1
+a,b,c = [1,2,3]
+
+print(a)
+print(b)
+print(c)
+
+#Ex 2
+d,e,f, *other, d = [1,2,3,4,5,6,7,8,9]
+print(d)
+print(e)
+print(f)
+print(other)
+print(d)
+#Check out what this happens
+
+
+#* None
+#It basically means nothing/null
+#There are use cases for assigning a variable to None
+
+
+#* Dictionary
+#A dataype and a data structure in python
+#An unordered key-value pair
+#Dictionaries contain objects with a key:value
+dictionary = {
+    'a': 1,
+    'b': 2,
+    'x': 3
+}
+
+print(dictionary['b'])
+#Output will grab where b is stored and grab the value
+
+# Ex
+dictionary = {
+    'a': [1,2,3],
+    'b': 'hello',
+    'x': True
+}
+print(dictionary['a'][1])
+
+#Ex
+my_list = [
+    {
+    'a': [1,2,3],
+    'b': 'hello',
+    'x': True
+    },
+    {
+    'a': [4,5,6],
+    'b': 'hello',
+    'x': True
+    }
+]
+print(my_list[0]['a'][2])
+
+#! When to use a List vs Dictionary
+#List has order
+# Simply an index and value
+#Dictionary has no order
+# Holds more information such as key
+# Can hold more information than Lists
+
+
+#* Dictionary Keys
+#The key must be something that is immutable
+dictionary = {
+    123: [1,2,3], #valid
+    True: 'hello', #valid
+    [100]: True #Not Valid
+}
